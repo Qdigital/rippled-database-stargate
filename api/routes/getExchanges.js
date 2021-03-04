ab1c82f054481ade516ad7de7cd628966ca7475a
Qdigital <48498797+Qdigital@users.noreply.github.com>
2021-03-04 14:41:46 +1100

    Initialize rpc

---
+++ api/routes/getExchanges.js
@@ -0,0 +1,277 @@
+'use strict'
+
+var Logger = require('../../lib/logger')
+var log = new Logger({scope: 'exchanges'})
+var smoment = require('../../lib/smoment')
+var utils = require('../../lib/utils')
+var hbase = require('../../lib/hbase')
+var intervals = [
+  '1minute',
+  '5minute',
+  '15minute',
+  '30minute',
+  '1hour',
+  '2hour',
+  '4hour',
+  '1day',
+  '3day',
+  '7day',
+  '1month',
+  '1year'
+]
+var PRECISION = 8
+
+function getExchanges(req, res) {
+  var params
+
+  function prepareOptions() {
+    var options = {
+      start: smoment(req.query.start || '2013-01-01'),
+      end: smoment(req.query.end),
+      interval: req.query.interval,
+      limit: Number(req.query.limit || 200),
+      base: {},
+      counter: {},
+      descending: (/true/i).test(req.query.descending) ? true : false,
+      reduce: (/true/i).test(req.query.reduce) ? true : false,
+      autobridged: (/true/i).test(req.query.autobridged) ? true : false,
+      format: (req.query.format || 'json').toLowerCase(),
+      marker: req.query.marker
+    }
+
+    var base = req.params.base.split(/[\+|\.]/) // any of +, |, or .
+    var counter = req.params.counter.split(/[\+|\.]/)
+
+    options.base.currency = base[0] ? base[0].toUpperCase() : undefined
+    options.base.issuer = base[1] ? base[1] : undefined
+
+    options.counter.currency = counter[0] ? counter[0].toUpperCase() : undefined
+    options.counter.issuer = counter[1] ? counter[1] : undefined
+
+    if (!options.base.currency) {
+      return {error: 'base currency is required', code: 400}
+    } else if (!options.counter.currency) {
+      return {error: 'counter currency is required', code: 400}
+    } else if (options.base.currency === 'XRP' && options.base.issuer) {
+      return {error: 'XRP cannot have an issuer', code: 400}
+    } else if (options.counter.currency === 'XRP' && options.counter.issuer) {
+      return {error: 'XRP cannot have an issuer', code: 400}
+    } else if (options.base.currency !== 'XRP' && !options.base.issuer) {
+      return {error: 'base issuer is required', code: 400}
+    } else if (options.counter.currency !== 'XRP' && !options.counter.issuer) {
+      return {error: 'counter issuer is required', code: 400}
+    }
+
+    if (!options.start) {
+      return {error: 'invalid start date format', code: 400}
+    } else if (!options.end) {
+      return {error: 'invalid end date format', code: 400}
+    }
+
+    if (options.interval) {
+      options.interval = options.interval.toLowerCase()
+    }
+    if (options.interval === 'week') {
+      options.interval = '7day'
+    }
+
+    if (isNaN(options.limit)) {
+      return {error: 'invalid limit: ' + options.limit, code: 400}
+    } else if (options.reduce && options.interval) {
+      return {error: 'cannot use reduce with interval', code: 400}
+    } else if (options.limit > 400) {
+      options.limit = 400
+    } else if (options.interval &&
+               intervals.indexOf(options.interval) === -1) {
+      return {error: 'invalid interval: ' + options.interval, code: 400}
+    }
+
+    return options
+  }
+
+  /**
+   * formatInterval
+   */
+
+  function formatInterval(ex) {
+    delete ex.rowkey
+    delete ex.sort_open
+    delete ex.sort_close
+
+    if (ex.open_time) {
+      ex.open_time = smoment(ex.open_time).format()
+    } else {
+      delete ex.open_time
+    }
+
+    if (ex.close_time) {
+      ex.close_time = smoment(ex.close_time).format()
+    } else {
+      delete ex.close_time
+    }
+
+    ex.start = smoment(ex.start).format()
+    ex.base_currency = params.base.currency
+    ex.base_issuer = params.base.issuer
+    ex.counter_currency = params.counter.currency
+    ex.counter_issuer = params.counter.issuer
+    ex.base_volume = ex.base_volume.toString()
+    ex.counter_volume = ex.counter_volume.toString()
+    ex.open = ex.open.toPrecision(PRECISION)
+    ex.high = ex.high.toPrecision(PRECISION)
+    ex.low = ex.low.toPrecision(PRECISION)
+    ex.close = ex.close.toPrecision(PRECISION)
+    ex.vwap = ex.vwap.toPrecision(PRECISION)
+  }
+
+  /**
+  * errorResponse
+  * return an error response
+  * @param {Object} err
+  */
+
+  function errorResponse(err) {
+    log.error(err.error || err)
+    if (err.code && err.code.toString()[0] === '4') {
+      res.status(err.code).json({
+        result: 'error',
+        message: err.error
+      })
+    } else {
+      res.status(500).json({
+        result: 'error',
+        message: 'unable to retrieve exchanges'
+      })
+    }
+  }
+
+  /**
+   * successResponse
+   * return a successful response
+   * @param {Object} exchanges
+   */
+
+  function successResponse(resp) {
+    var filename
+
+    if (resp.marker) {
+      utils.addLinkHeader(req, res, resp.marker)
+    }
+
+    if (params.format === 'csv') {
+      filename = 'exchanges - ' +
+        params.base.currency + '-' +
+        params.counter.currency +
+        '.csv'
+
+      // ensure consistent order and
+      // inclusion of all fields
+      if (resp.rows.length &&
+         (params.reduce || params.interval)) {
+
+        resp.rows[0] = {
+          open: resp.rows[0].open,
+          high: resp.rows[0].high,
+          low: resp.rows[0].low,
+          close: resp.rows[0].close,
+          vwap: resp.rows[0].vwap,
+          count: resp.rows[0].count,
+          base_currency: resp.rows[0].base_currency,
+          base_issuer: resp.rows[0].base_issuer,
+          base_volume: resp.rows[0].base_volume,
+          counter_currency: resp.rows[0].counter_currency,
+          counter_issuer: resp.rows[0].counter_issuer,
+          counter_volume: resp.rows[0].counter_volume,
+          open_time: resp.rows[0].open_time,
+          close_time: resp.rows[0].close_time,
+          start: resp.rows[0].start
+        }
+
+      } else if (resp.rows.length) {
+        resp.rows[0] = {
+          base_currency: resp.rows[0].base_currency,
+          base_issuer: resp.rows[0].base_issuer,
+          base_amount: resp.rows[0].base_amount,
+          counter_amount: resp.rows[0].counter_amount,
+          counter_currency: resp.rows[0].counter_currency,
+          counter_issuer: resp.rows[0].counter_issuer,
+          rate: resp.rows[0].rate,
+          executed_time: resp.rows[0].executed_time,
+          ledger_index: resp.rows[0].ledger_index,
+          buyer: resp.rows[0].buyer,
+          seller: resp.rows[0].seller,
+          taker: resp.rows[0].taker,
+          provider: resp.rows[0].provider,
+          autobridged_currency: resp.rows[0].autobridged_currency,
+          autobridged_issuer: resp.rows[0].autobridged_issuer,
+          offer_sequence: resp.rows[0].offer_sequence,
+          tx_type: resp.rows[0].tx_type,
+          tx_index: resp.rows[0].tx_index,
+          node_index: resp.rows[0].node_index,
+          tx_hash: resp.rows[0].tx_hash
+        }
+      }
+
+      res.csv(resp.rows, filename)
+    } else {
+      res.json({
+        result: 'success',
+        count: resp.rows.length,
+        marker: resp.marker,
+        exchanges: resp.rows
+      })
+    }
+  }
+
+  params = prepareOptions()
+
+  if (params.error) {
+    errorResponse(params)
+
+  } else {
+    log.info(params.base.currency, params.counter.currency)
+
+    hbase.getExchanges(params, function(err, resp) {
+      if (err && err === 'too many rows') {
+        errorResponse({
+          code: 400,
+          error: 'too many exchanges, use a smaller interval'
+        })
+
+      } else if (err) {
+        errorResponse(err)
+
+      } else if (params.reduce) {
+        formatInterval(resp.reduced)
+        resp.rows = [resp.reduced]
+        successResponse(resp)
+
+      } else {
+        if (params.interval) {
+          resp.rows.forEach(formatInterval)
+
+        } else {
+          resp.rows.forEach(function(ex) {
+            delete ex.rowkey
+            delete ex.time
+            delete ex.client
+
+            ex.executed_time = smoment(ex.executed_time).format()
+            ex.base_currency = params.base.currency
+            ex.base_issuer = params.base.issuer
+            ex.counter_currency = params.counter.currency
+            ex.counter_issuer = params.counter.issuer
+            ex.base_amount = ex.base_amount.toString()
+            ex.counter_amount = ex.counter_amount.toString()
+            ex.rate = ex.rate.toPrecision(PRECISION)
+          })
+        }
+
+        successResponse(resp)
+      }
+    })
+  }
+}
+
+
+module.exports = getExchanges
---
+++ api/server.js
@@ -0,0 +1,162 @@
+/* eslint no-unused-vars: ["error", { "args": "after-used" }] */
+'use strict'
+
+var express = require('express')
+var bodyParser = require('body-parser')
+var compression = require('compression')
+var cors = require('cors')
+var routes = require('./routes')
+var map = require('./apiMap')
+var json2csv = require('nice-json2csv')
+var favicon = require('serve-favicon')
+
+/**
+ * filterDuplicateQueryParams
+ * NOTE: this only works if we dont pass
+ * an array as a query param intentionally
+ */
+
+function filterDuplicateQueryParams(req, res, next) {
+
+  for (var key in req.query) {
+    if (Array.isArray(req.query[key])) {
+      req.query[key] = req.query[key][0]
+    }
+  }
+
+  next()
+}
+
+/**
+ * Server
+ */
+
+function Server(options) {
+
+  var app = express()
+  var cacheControl = ''
+  var server
+
+  /**
+  * setCacheControl
+  */
+
+  function setCacheControl(req, res, next) {
+    res.setHeader('Cache-Control', cacheControl)
+    next()
+  }
+
+  Object.keys(options.cacheControl || {}).forEach(function(key) {
+    cacheControl += key + '=' + options.cacheControl[key] + ', '
+  })
+
+  console.log('CACHE: ' + (cacheControl || 'none set'))
+
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({extended: true}))
+  app.use(json2csv.expressDecorator)
+  app.use(cors())
+  app.use(filterDuplicateQueryParams)
+  app.use(favicon(__dirname + '/favicon.png'))
+  app.use(compression())
+  app.use(setCacheControl)
+
+  app.get('/v2/health/:aspect?', routes.checkHealth)
+  app.get('/v2/gateways/:gateway?', routes.gateways.Gateways)
+  app.get('/v2/gateways/:gateway/assets/:filename?', routes.gateways.Assets)
+  app.get('/v2/currencies/:currencyAsset?', routes.gateways.Currencies)
+  app.get('/v2/capitalization/:currency', routes.capitalization)
+  app.get('/v2/active_accounts/:base/:counter', routes.activeAccounts)
+  app.get('/v2/network/exchange_volume', routes.network.exchangeVolume)
+  app.get('/v2/network/payment_volume', routes.network.paymentVolume)
+  app.get('/v2/network/issued_value', routes.network.issuedValue)
+  app.get('/v2/network/external_markets', routes.network.externalMarkets)
+  app.get('/v2/network/xrp_distribution', routes.network.xrpDistribution)
+  app.get('/v2/network/top_markets/:date?', routes.network.topMarkets)
+  app.get('/v2/network/top_currencies/:date?', routes.network.topCurrencies)
+  app.get('/v2/network/fees', routes.network.getFees)
+  app.get('/v2/network/fee_stats', routes.network.getFeeStats)
+  app.get('/v2/network/topology', routes.network.getTopology)
+  app.get('/v2/network/topology/nodes', routes.network.getNodes)
+  app.get('/v2/network/topology/nodes/:pubkey', routes.network.getNodes)
+  app.get('/v2/network/topology/links', routes.network.getLinks)
+  app.get('/v2/network/validators', routes.network.getValidators)
+  app.get('/v2/network/validators/:pubkey', routes.network.getValidators)
+  app.get('/v2/network/validators/:pubkey/validations',
+          routes.network.getValidations)
+  app.get('/v2/network/validators/:pubkey/reports',
+          routes.network.getValidatorReports)
+  app.get('/v2/network/validator_reports', routes.network.getValidatorReports)
+  app.get('/v2/network/validations', routes.network.getValidations)
+  app.get('/v2/network/rippled_versions', routes.network.getVersions)
+  app.get('/v2/last_validated', routes.getLastValidated)
+  app.get('/v2/transactions/', routes.getTransactions)
+  app.get('/v2/transactions/:tx_hash', routes.getTransactions)
+  app.get('/v2/ledgers/:ledger_param?', routes.getLedger)
+  app.get('/v2/ledgers/:ledger_hash/validations',
+          routes.network.getLedgerValidations)
+  app.get('/v2/ledgers/:ledger_hash/validations/:validation_pubkey',
+          routes.network.getLedgerValidations)
+  app.get('/v2/accounts', routes.accounts)
+  app.get('/v2/accounts/:address', routes.getAccount)
+  app.get('/v2/accounts/:address/transactions/:sequence', routes.accountTxSeq)
+  app.get('/v2/accounts/:address/transactions', routes.accountTransactions)
+  app.get('/v2/accounts/:address/balances', routes.accountBalances)
+  app.get('/v2/accounts/:address/payments', routes.accountPayments)
+  app.get('/v2/accounts/:address/reports/:date?', routes.accountReports)
+  app.get('/v2/accounts/:address/escrows', routes.accountEscrows)
+  app.get('/v2/accounts/:address/payment_channels',
+          routes.accountPaymentChannels)
+  app.get('/v2/accounts/:address/balance_changes', routes.getChanges)
+  app.get('/v2/accounts/:address/exchanges', routes.accountExchanges)
+  app.get('/v2/accounts/:address/exchanges/:base', routes.accountExchanges)
+  app.get('/v2/accounts/:address/exchanges/:base/:counter',
+          routes.accountExchanges)
+  app.get('/v2/accounts/:address/orders', routes.accountOrders)
+  app.get('/v2/accounts/:address/stats/:family', routes.accountStats)
+  app.get('/v2/accounts', routes.accounts)
+  app.get('/v2/payments/:currency?', routes.getPayments)
+  app.get('/v2/exchanges/:base/:counter', routes.getExchanges)
+  app.get('/v2/exchange_rates/:base/:counter', routes.getExchangeRate)
+  app.get('/v2/normalize', routes.normalize)
+  app.get('/v2/reports/:date?', routes.reports)
+  app.get('/v2/stats', routes.stats)
+  app.get('/v2/stats/:family', routes.stats)
+  app.get('/v2/stats/:family/:metric', routes.stats)
+  app.get('/v2/maintenance/:domain', routes.maintenance)
+  app.get('/v2/estimate', routes.estimate)
+  app.get('/v2/xrp_index', routes.xrpIndex)
+  app.get('/v2/xrp_index/ticker', routes.xrpIndexTicker)
+
+  // index page
+  app.get('/', map.generate)
+  app.get('/v2', map.generate)
+
+  // 404
+  app.get('*', map.generate404)
+  app.post('*', map.generate404)
+
+
+  // start the server
+  server = app.listen(options.port)
+  console.log('Ripple Data API running on port: ' + options.port)
+
+  // log error
+  server.on('error', function(err) {
+    console.log(err)
+  })
+
+  // log close
+  server.on('close', function() {
+    console.log('server on port: ' + options.port + ' closed')
+  })
+
+  this.close = function() {
+    if (server) {
+      server.close()
+      console.log('closing API on port: ' + options.port)
+    }
+  }
+}
+
+module.exports = Server
